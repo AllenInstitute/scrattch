f81814de5b2518b700223b36a5ba5d0aa11817da
Lucas Gray <soulcatalyst@gmail.com>
2016-09-13 10:59:48 -0700

    Removing most of the built-in dataset to make the package much faster to download. The rest of the data will be shifted to a separate package (Tasic2016Data).

--- R/data.R
+++ R/data.R
@@ -1,11 +1,11 @@
 #' Annotations for V1 single-cell transcriptomic datasets
 #' 
-#' A dataset consisting of descriptive annotation data for all 1679
-#' cells characterized in Tasic, et al. (2015).
+#' A dataset consisting of descriptive annotation data for a subset of the
+#' cells characterized in Tasic, et al. (2016).
 #' 
-#' @format A Data Frame with 11 columns and 1679 rows
+#' @format A Data Frame with 11 columns and 282 rows
 #' \describe{
-#'  \item{filename}{A unique filename that uniquely corresponds to a single cell. Matches column names in v1_rpkm}
+#'  \item{sample_id}{A unique cell identifier that uniquely corresponds to a single cell. Matches column names in v1_data}
 #'  \item{short_name}{A unique short cell identifier that is used in the Allen Brain Institute Vignette}
 #'  \item{cre_id}{Numeric ID corresponding to the Cre line that the cell was harvested from}
 #'  \item{cre_color}{Character value for a color corresponding to the cre_id}
@@ -21,14 +21,14 @@
 
 #' RPKM data for single-cell transcriptomic datasets
 #' 
-#' A dataset consisting of RPKM values for all 1679 cells
-#' characterized in Tasic, et al. (2015).
+#' A dataset consisting of RPKM values for a subset of the cells
+#' characterized in Tasic, et al. (2016).
 #' 
 #' Each row corresponds to a single gene symbol. Each column corresponds to a single cell.
 #' 
-#' @format A Data Frame with 1670 columns (cells) and 24,057 rows (genes)
+#' @format A Data Frame with 283 columns (cells) and 24,057 rows (genes)
 #' \describe{
-#'  \item{sample_id}{The first column, a list of gene symbols}
+#'  \item{gene}{The first column, a list of gene symbols}
 #'  \item{other columns}{Each subsequent column is named using a unique cell identifier that corresponds to the filename column in the v1_anno annotations table.}
 #' }
 #' 
