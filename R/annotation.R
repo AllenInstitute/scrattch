c6798e3591a68ad7d6f4952a2d2af45d507342a8
Lucas Gray <soulcatalyst@gmail.com>
2017-03-02 11:51:06 -0800

    better dplyr compatibility for annotation functions, and new minimum value scaling for values_to_colors

--- R/annotation.R
+++ R/annotation.R
@@ -1,38 +1,61 @@
-annotate_numeric <- function(x, name = NULL, logscale = F, colorset = c("darkblue","white","red")) {
-  
+annotate_numeric <- function (df, 
+                              col = NULL, base = NULL, 
+                              scale = "log10", na_val = 0, 
+                              colorset = c("darkblue", "white", "red")) 
+{
   library(scrattch)
   library(dplyr)
   
-  if(!is.numeric(x)) {
-    x <- as.numeric(x)
+  if(is.null(base)) {
+    base <- col
   }
   
-  annotations <- data.frame(label = unique(x)) %>%
-    arrange(label) %>%
+  if (!is.numeric(df[[col]])) {
+    df[[col]] <- as.numeric(df[[col]])
+    df[[col]][is.na(df[[col]])] <- na_val
+  }
+  
+  x <- df[[col]]
+  
+  annotations <- data.frame(label = unique(x)) %>% 
+    arrange(label) %>% 
     mutate(id = 1:n())
   
-  if(logscale == T) {
-    colors <- values_to_colors(log10(annotations$label + 1),colorset = colorset)
+  if (scale == "log10") {
+    colors <- values_to_colors(log10(annotations$label + 1), colorset = colorset)
+  } else if(scale == "log2") {
+    colors <- values_to_colors(log2(annotations$label + 1), colorset = colorset)
+  } else if(scale == "zscore") {
+    colors <- values_to_colors(scale(annotations$label), colorset = colorset)
   } else {
-    colors <- values_to_colors(annotations$label,colorset = colorset)
+    colors <- values_to_colors(annotations$label, colorset = colorset)
   }
+  annotations <- mutate(annotations, color = colors)
+  names(annotations) <- paste0(base, c("_label", "_id", "_color"))
   
-  annotations <- cbind(annotations, color = colors, stringsAsFactors = F)
-  
-  names(annotations) <- paste0(name,c("_label","_id","_color"))
-  
-  return(annotations)
-  
+  names(df)[names(df) == col] <- paste0(base,"_label")
+  df <- left_join(df, annotations, by = paste0(base,"_label"))
+  df
 }
 
-annotate_categorical <- function(x, name = NULL, sort_label = T, colorset = "rainbow", color_order = "sort") {
+annotate_categorical <- function(df, 
+                                 col = NULL, base = NULL, 
+                                 sort_label = T, na_val = "ZZ_Missing", 
+                                 colorset = "rainbow", color_order = "sort") {
   
   library(dplyr)
   library(viridis)
-
-  if(!is.character(x)) {
-    x <- as.character(x)
+  
+  if(is.null(base)) {
+    base <- col
   }
+  
+  if(!is.character(df[[col]])) {
+    df[[col]] <- as.character(df[[col]])
+    df[[col]][is.na(df[[col]])] <- na_val
+  }
+  
+  x <- df[[col]]
   
   annotations <- data.frame(label = unique(x), stringsAsFactors = F)
   
@@ -63,10 +86,24 @@ annotate_categorical <- function(x, name = NULL, sort_label = T, colorset = "rai
     
   }
   
-  annotations <- cbind(annotations, color = colors, stringsAsFactors = F)
+  annotations <- mutate(annotations, color = colors)
   
-  names(annotations) <- paste0(name,c("_label","_id","_color"))
-  
-  return(annotations)
+  names(annotations) <- paste0(base,c("_label","_id","_color"))
+  names(df)[names(df) == col] <- paste0(base,"_label")
+  df <- left_join(df, annotations, by = paste0(base,"_label"))
+  df
+}
 
+group_annotations <- function(df, keep_order = TRUE) {
+  labels <- names(df)[grepl("_label",names(df))]
+  if(!keep_order) {
+    labels <- labels[order(labels)]
+  }
+  bases <- sub("_label","",labels)
+  
+  anno_cols <- c(paste0(rep(bases,each=3),c("_id","_label","_color")))
+  extras <- setdiff(names(df),anno_cols)
+  
+  anno <- select(df,one_of(c("sample_id",anno_cols,extras)))
+  
 }
