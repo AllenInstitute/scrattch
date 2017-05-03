ca3dfc1f4dc891678643af745db3cc5b30f88366
Lucas Gray <soulcatalyst@gmail.com>
2017-05-03 13:00:09 -0700

    update_scrattch now pulls from AI bitbucket

--- R/scrattch.R
+++ R/scrattch.R
@@ -1,4 +1,4 @@
 #' Pull the current version of scrattch from GitHub
 update_scrattch <- function() {
-  devtools::install_github("hypercompetent/scrattch",auth_token="ed22ec6b1d333fcb9d4d78a1e7ebd29ec72d0048",build_vignettes = T)
+  devtools::install_git("http://stash.corp.alleninstitute.org/scm/tran/scrattch.git")
 }
